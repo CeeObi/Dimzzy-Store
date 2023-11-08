523b9c52800cf8edf5bb7af558a1309eb764f83b
CeeObi <140174587+CeeObi@users.noreply.github.com>
2023-11-09 00:53:50 +1100

    update header, navbar, login, register and homelayout page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,15 +17,12 @@ const Login = () =>{
         <div>
         <button type="button" className="btn btn-secondary btn-block">
                 guest user
-        </button>
+        </button>        
+        </div>
         <p className="text-center">
             Not a member yet? <Link to="/register" className="ml-2 link link-hover link-primary capitalize"> register</Link>
         </p>
-        </div>
-
-
     </Form>
-
     </section>
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,26 @@
+import { Form, Link } from "react-router-dom";
+import {FormInput, SubmitBtn} from "../components"
+
 const Register = () =>{
-    return <h1 className="text-4xl"> Register</h1>
+    return <section className="h-screen grid place-items-center">
+        <Form method="POST" className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4">
+            <h4 className="text-center font-bold text-3xl">Register</h4>
+            <div>
+                <FormInput type="text" name="username" label="username"  />
+                <FormInput type="email" name="email" label="email"/>
+                <FormInput type="password" name="password" label="password"/>
+            </div>
+            
+            <div className="mt-4">
+                <SubmitBtn text="Register"/>            
+            </div>
+            <div>
+                <p className="text-center">
+                    Already a member? <Link to="/login" className="ml-2 link link-hover link-primary capitalize"> login</Link>
+                </p>
+            </div>
+        </Form>
+    </section>
 }
 
 export default Register;

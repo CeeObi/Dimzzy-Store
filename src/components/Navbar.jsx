1ee73ddf14457b1e153cdc6d23b48bd22cdcd8d4
CeeObi <140174587+CeeObi@users.noreply.github.com>
2023-11-09 01:26:42 +1100

    update navlinks dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import {BsCart3, BsMoonFill, BsSunFill} from 'react-icons/bs';
 import {FaBarsStaggered} from 'react-icons/fa6';
 import {NavLink} from 'react-router-dom';
+import NavLinks from './NavLinks';
+
 
 
 const Navbar = () => {
@@ -19,14 +21,15 @@ const Navbar = () => {
                     <label tabIndex={0} className='btn btn-ghost lg:hidden'>
                         <FaBarsStaggered className="h-6 w-6" />
                     </label>
-                    <ul tabIndex={0} className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200'>
-                        nav links
+                    <ul tabIndex={0} className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52'>
+                        <NavLinks />
                     </ul>
                 </div>
-            </div>
-            
+            </div>            
             <div className="navbar-center hidden lg:flex">
-                <ul className='menu menu-horizontal'>nav links</ul>
+                <ul className='menu menu-horizontal'>
+                <NavLinks />
+                </ul>
             </div>
             <div className="navbar-end">
                 {/*THEME ICON */}

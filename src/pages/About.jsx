36187642dbb1b80738476d3ae02bfd0a012beefa
CeeObi <140174587+CeeObi@users.noreply.github.com>
2023-11-09 12:31:49 +1100

    update about page complete

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,21 @@
 const About = () =>{
-    return <h1 className="text-4xl"> About</h1>
+    return <>
+        <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center ">
+            <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+                We love
+            </h1>
+            <div className="stats bg-primary shadow">
+                <div className="stat">
+                    <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+                        Dimzzy
+                    </div>
+                </div>
+            </div>
+        </div>
+        <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam porro doloribus eveniet vitae fugiat doloremque corrupti odit sint. Cupiditate fuga veniam laboriosam, saepe est maiores harum modi, sit tempore expedita incidunt commodi voluptate rem optio quas repellendus illum. Odio, sint animi, nihil perspiciatis aperiam molestiae, accusantium ea ab neque totam quibusdam dolorem accusamus. Delectus voluptatibus minus expedita molestias dolorum optio!
+        </p>
+    </>
 }
 
 export default About;

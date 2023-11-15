03169124e4fcb67c57dcf2458c2e462ad0ef5762
CeeObi <140174587+CeeObi@users.noreply.github.com>
2023-11-16 00:01:33 +1100

    complete checkout form page

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
-import { Form } from 'react-router-dom';
+import { Form, redirect } from 'react-router-dom';
 import FormInput from './FormInput';
 import SubmitBtn from './SubmitBtn';
+import { customFetch, formatPrice } from '../utils';
+import { clearCart } from '../features/cart/cartSlice';
+import { toast } from 'react-toastify';
 
 
-const Action = (store) => async () => {
-    console.log(store);
-    return null
+const Action = (store) => async ({request}) => {
+    const formData = await request.formData();
+    const {name, address} = Object.fromEntries(formData);
+    const user = store.getState().userState.user;
+    const {cartItems, orderTotal, numItemsInCart} = store.getState().cartState
+    const info ={
+        name,address, chargeTotal:orderTotal, orderTotal:formatPrice(orderTotal), cartItems, numItemsInCart
+    }
+    try {
+        const response = await customFetch.post("/orders",{data:info},{
+            headers: {
+                Authorization: `Bearer ${user.token}`
+            }
+        })
+        store.dispatch(clearCart());
+        toast.success("Order has been placed successfully!");
+        return redirect("/orders");
+    } catch (error) {
+        console.log(error)
+        const errorMessage = error?.response?.data?.error?.message || "There was an error placing your order!"
+        toast.error(errorMessage);
+        return null;
+    }
 }
 
 
